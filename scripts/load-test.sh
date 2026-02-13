23ec810bf727316e64e871742b9a104602d5273c
Ben Greene <BenJGreene@gmail.com>
2026-02-13 18:32:28 +0000

    Scripts of load-test to 127.0.0.1 and kafka refs to any version

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -4,7 +4,7 @@ set -e
 # Configuration
 DURATION="30s"
 CONCURRENCY=100
-BASE_URL="http://localhost:8080/cache/customers"
+BASE_URL="http://127.0.0.1:8080/cache/customers"
 
 
 # 0. Ensure local cargo bin is in PATH
